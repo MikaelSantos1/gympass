928127f87f76733be20c9571362b7b5329e504f3
MikaelSantos1 <mikaelsantos120@outlook.com>
2024-04-14 13:41:08 -0300

    RBAC

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,27 @@ import { register } from "./http/controllers/users/register";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { gymRoutes } from "./http/controllers/gyms/routes";
 import { checkInRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie);
 app.register(usersRoutes);
 app.register(gymRoutes);
 app.register(checkInRoutes);
-
-app.register(fastifyJwt, {
-  secret: env.JWT_SECRET,
-});
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
